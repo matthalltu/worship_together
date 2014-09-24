bbb08e06c826deca52a8d790bc321a9a0072037d
matthalltu <matthalltu@gmail.com>
2014-09-24 03:41:31 +0000

    At curent working version - New Resource for logging in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     end
 
     def create
-	@user = User.new(params.require(:user).permit(:name, :email, :password))
+	@user = User.new(user_params)
 	if @user.save
 	    flash[:success] = "Welcome to the site, #{@user.name}"
 	    redirect_to @user
@@ -21,17 +21,20 @@ class UsersController < ApplicationController
     def show
 	@user = User.find(params[:id])
     rescue
-	flash[:danger] = "Unable to find user"
-	redirect_to users_path
+	    flash[:danger] = "Unable to find user"
+	    redirect_to users_path
     end
 
     def edit
 	@user = User.find(params[:id])
+    rescue
+	    flash[:danger] = "Unable to find user"
+	    redirect_to users_path
     end
   
   def update 
     @user = User.find(params[:id])
-    if @user.update_attributes(params.require(:user).permit(:name, :email, :password))
+    if @user.update_attributes(user_params)
 	    flash[:success] = "Welcome to the site, #{@user.name}"
 	    redirect_to @user
 	else
@@ -39,5 +42,17 @@ class UsersController < ApplicationController
     	    render 'edit'
 	end
     end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "#{@user.name} removed from the site" 
+    redirect_to users_path
+  end
   
+  private 
+	
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
+  	end
 end
