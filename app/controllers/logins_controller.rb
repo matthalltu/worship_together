bf48ce4d94e0a4c548b1454bd08f7065b9d2f580
matthalltu <matthalltu@gmail.com>
2014-09-26 05:01:52 +0000

    At current working version - 09/26/14

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,19 @@
+class LoginsController < ApplicationController
+	def new
+	end
+	
+	def create
+		user = User.find_by(name: params[:username]) 
+		if user && user.password == params[:password]
+			flash[:success] = 'Logged In'
+			redirect_to users_path
+		else
+			flash.now[:danger] = 'Invalid username or password'
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
+
+end
